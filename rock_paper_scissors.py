8f48778ce01b209508c2504475b0e1bcf8af5abd
Joshua Darovitz <joshdarovitz@gmail.com>
2025-05-30 14:10:54 -0400

    Final RPS program file, finished rps_save_config for saving results to JSON

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,13 +3,21 @@ Welcome to Rock Paper Scissors!
 
 This program takes a user input (rock, paper, scissors)
 A random computer input of the same 3 choices 
-And determines a winner:rock > scissors, scissors > paper, paper > rock
+And determines a winner:
+Rock beats paper, paper beats rock, scissors beats paper
+
 Print the winner and save it to persistent memory along with choices
 Asks user if they would like to play again or load saved data
+First to 5 wins is the Grand Winner!
 """
 import random
+from rps_save_config import (
+    clear_saves,
+    save_data,
+    return_saved_data
+)
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 CONTINUE_PROMPT = "Do you want to play again? ('y' for yes or 'n' for no)"
 END_PROGRAM = {'n', 'no', 'stop', 'quit', 'exit', 'end'}
 GAME_HISTORY = []
@@ -25,7 +33,7 @@ def will_continue(message):
     """
     Function to validate user input if they wish to continue
     Repeat until returns a correct user input of 'n' or 'no' 
-    param: message, the message printed to the user requesting input
+    PARAM: message, the message printed to the user requesting input
     """
     while True:
         point_to_prompt(message)
@@ -40,12 +48,12 @@ def display_winner(player, computer):
     """
     Display choices and result of game in a clean way
     Return a 'win', 'loss', or 'tie' to save to outside file
-    param: player, the player's choice (rock, paper, scissors)
+    PARAM: player, the player's choice (rock, paper, scissors)
     computer, the computer's choice (rock, paper, scissors)
     """
     #to reduce characters in prompt
-    play = player.title()
-    comp = computer.title()
+    play = player.upper()
+    comp = computer.upper()
     point_to_prompt(f'You chose {play}, computer chose {comp}\n')
 
     wins_against = {
@@ -76,19 +84,38 @@ while True:
 
     computer_choice = random.choice(VALID_CHOICES)
 
-    WINNER = display_winner(choice, computer_choice)
+    RESULT = display_winner(choice, computer_choice)
     GAME_HISTORY.append({
         'user_choice': choice,
         'computer_choice': computer_choice,
-        'result': WINNER,
+        'result': RESULT,
     })
+    save_data(choice, computer_choice, RESULT)
 
     if not will_continue(CONTINUE_PROMPT):
-        point_to_prompt("Game History this round:\n")
+        point_to_prompt("Game History this round:")
         for i, game in enumerate(GAME_HISTORY, 1):
-            print(f'Round {i}: You chose {game['user_choice']}, '
-                  f'Computer chose {game['computer_choice']}, '
-                  f'Result: {game['result'].upper()}')
+            print(f'Round {i}: You chose {game['user_choice'].upper()}, '
+                  f'Computer chose {game['computer_choice'].upper()}, '
+                  f'Result: {game['result'].upper()}\n')
+
+        point_to_prompt('Please type: ')
+        point_to_prompt('1 to view your saved game history')
+        point_to_prompt('2 to clear out all your saves')
+        point_to_prompt('or n to quit? ')
+        save_choice = input()
+
+        while save_choice not in END_PROGRAM:
+            match save_choice:
+                case '1':
+                    print(return_saved_data())
+                    break
+                case '2':
+                    clear_saves()
+                    break
+                case _:
+                    print('That was not a valid option.')
+                    save_choice = input('Please pick 1, 2 or n to quit: ')
         break
 
     #add a way to distinguish the different rounds
---
+++ rps_save_config.py
@@ -0,0 +1,72 @@
+"""
+File to help import/export data from main RPS program to JSON file
+Used to save games, delete all saves and retrieve save information
+Retrieve info such as # of times won with rock, paper, scissors, etc
+"""
+from collections import defaultdict
+import json
+import os
+
+FILE_NAME = 'rps_save_data.json'
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_RESULTS = ['win', 'lose', 'tie']
+
+def load_saved_data():
+    """
+    Load existing data from JSON
+    Used to pull dictionaries from JSON list of saved data
+    """
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME, 'r') as file:
+            return json.load(file)
+    return []
+
+def clear_saves():
+    """
+    Function to empty and clear out the list in the JSON file
+    Empty all saved data and start new list
+    """
+
+    with open(FILE_NAME, 'w') as file:
+        json.dump([], file)
+
+def save_data(user_choice, computer_choice, result):
+    """
+    Save data from program in JSON file
+    Take data as params and write this data as a dict in the JSON list
+    PARAMS: user_choice: the user choice of rock, paper, scissors
+    computer_choice: the computer's choice of rock, paper, scissors
+    result: the result of the game (win, lose, tie)
+    """
+    saved_data = load_saved_data()
+
+    saved_data.append({
+        'user_choice': user_choice,
+        'computer_choice' : computer_choice,
+        'result' : result
+    })
+
+    with open(FILE_NAME, 'w') as file:
+        json.dump(saved_data, file, indent=4)
+
+def return_saved_data():
+    """
+    Take saved data and return a new string that displays:
+    Number of wins, losses and ties with each pick (rock, paper, scissors)
+    """
+    stats = defaultdict(lambda: {'win': 0, 'lose': 0, 'tie': 0})
+
+    for data in load_saved_data():
+        choice = data['user_choice']
+        result = data['result']
+        if choice in VALID_CHOICES and result in VALID_RESULTS:
+            stats[choice][result] += 1
+
+    ouput = []
+    for choice in VALID_CHOICES:
+        ouput.append(f'{choice.upper()}: \n'
+                     f'    total wins: {stats[choice]['win']}\n'
+                     f'    total losses: {stats[choice]['lose']}\n'
+                     f'    total ties: {stats[choice]['tie']}\n'
+                     )
+    return ''.join(ouput)
